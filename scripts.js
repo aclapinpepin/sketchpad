53f66bc983105710e8a2ff96556a876850407273
aclapinpepin <aclapinpepin@gmail.com>
2015-07-02 15:11:39 -0400

    Fixed hover when reseting grid

--- scripts.js
+++ scripts.js
@@ -1,15 +1,24 @@
-var insertSquares = function() {
-    for (i = 0; i < 16; i++) {
+var DEFAULT_GRID_SIZE = 16;
 
-        var row = "<div>";
+var insertSquares = function(size) {
+    var dimension  = 960;
+    var squareSize = (960 / size) - 2 // Adjust for border size
+    var wrapper    = $( ".wrapper" ).html( "" );
 
-    for (j = 0; j < 16; j++) {
-        row += "<div class='square'>test</div>";
-    }
-        row += "</div>";
-        $( "#container" ).append( row );
+    // clearGrid();
+
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            wrapper.append( $( "<div></div>" ).addClass( "square" )
+            .height(squareSize).width(squareSize));
+        }
+        wrapper.append( $( "<div></div>" ).css("clear", "both")); // break line
     }
 };
+
+var clearGrid = function() {
+    $( ".row" ).remove();
+}
 
 var resetGrid = function() {
     $( ".square" ).css("background-color", "#045");
@@ -19,16 +28,37 @@ var resetGrid = function() {
 var customGrid = function() {
     var newSize = prompt("Please choose how many squares\
                          you want in the grid:");
+
+    return newSize;
+    // insertSquares(newSize);
 }
 
+var changeOnHover = function() {
+    $( ".square" ).mouseenter(function() {
+        $( this ).css("background-color", "#fff");
+    });
+};
+
 $( document ).ready(function() {
-    insertSquares();
+    insertSquares(DEFAULT_GRID_SIZE);
+    changeOnHover();
 
     $( "#reset" ).click(function() {
-      resetGrid();
-    })
-
-    $( ".square" ).hover(function() {
-      $( this ).css("background-color", "#fff");
+        // $( ".square" ).unbind();
+        resetGrid();
+        changeOnHover();
     });
+
+    $ ( "#custom_grid" ).click(function() {
+        // clearGrid();
+        var size = customGrid();
+        insertSquares(size);
+    });
+
+
+    // hover(function() {
+    //     $( this ).addClass( "hover" );
+
+        // css("background-color", "#fff", "!important");
+    // });
 });
